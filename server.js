26a4d40dfa83b1805fa291ac0d2f2643669e3344
Subhamay-Dey <ncdey1966@gmail.com>
2024-12-26 08:45:21 +0530

    express js setup done ✅

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express"
+import "dotenv/config"
+
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.get("/", (req, res) => {
+    return res.json({message: "Hello It's working..."});
+})
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
